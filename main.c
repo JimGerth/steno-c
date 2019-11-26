e361397b29dbe96355c08806064cf5ea77ae18c2
JimGerth <jim.frederic.gerth@gmail.com>
2019-11-26 15:14:36 +0100

    added loop to print out command line arguments

--- main.c
+++ main.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
+
+	// printing all the command line arguments excluding the name of the programm
+	printf("command line arguments: ");
+	for (int i = 1; i < argc; i++) {
+		printf("%s ", argv[i]);
+	}
+
 	return 0;
 }
